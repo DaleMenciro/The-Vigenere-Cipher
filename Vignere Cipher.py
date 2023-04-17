48fd5395e35ff54b02e86f7003d3e43fbb5b7373
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 15:44:59 +0800

    def function is utilized for vignere cipher

--- Vignere Cipher.py
+++ Vignere Cipher.py
@@ -1,3 +1,4 @@
 #Encrypts plaintext using the Vigenère cipher with the given key.
+def vigenere_cipher(plaintext,key):
 #plaintext-- error message will appear if it's not in all uppercase letters, no spaces
 #The ciphertext produced by encrypting plaintext using the Vigenère cipher with the given key.
