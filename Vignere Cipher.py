4fb7c6c397eb0eefaaf2d2576feb80feef160072
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 15:58:19 +0800

    calculates the length of the key, which will be also used in decrypting

--- Vignere Cipher.py
+++ Vignere Cipher.py
@@ -1,6 +1,6 @@
 #Encrypts plaintext using the Vigenère cipher with the given key.
 def vigenere_cipher(plaintext,key):
     ciphertext= ""
-    
+    key_len= len(key)
 #plaintext-- error message will appear if it's not in all uppercase letters, no spaces
 #The ciphertext produced by encrypting plaintext using the Vigenère cipher with the given key.
