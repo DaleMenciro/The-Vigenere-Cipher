909112500178560a8fe9801b7a0b1904a7724884
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 16:03:22 +0800

    checks if the plaintext is in all uppercase letters. If not, ValueError will be raised, informing the user.

--- Vignere Cipher.py
+++ Vignere Cipher.py
@@ -3,5 +3,7 @@ def vigenere_cipher(plaintext,key):
     ciphertext= ""
     key_len= len(key)
     for i, char in enumerate(plaintext):
-#plaintext-- error message will appear if it's not in all uppercase letters, no spaces
+        #plaintext-- error message will appear if it's not in all uppercase letters, no spaces
+        if not char.isalpha() or not char.isupper():
+            raise ValueError("The Plaintext must be in all uppercase letters wit no spaces only")
 #The ciphertext produced by encrypting plaintext using the Vigenère cipher with the given key.
