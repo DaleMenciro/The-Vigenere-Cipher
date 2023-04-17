2bf9961bf08b263ef8f1494acb29b40a61713b3b
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 15:39:56 +0800

    Pseudocode for Vignere Cipher

---
+++ Vignere Cipher.py
@@ -0,0 +1,3 @@
+#Encrypts plaintext using the Vigenère cipher with the given key.
+#plaintext-- error message will appear if it's not in all uppercase letters, no spaces
+#The ciphertext produced by encrypting plaintext using the Vigenère cipher with the given key.
