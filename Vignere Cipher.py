f7a50583831bffb86c89aac97ad041c02e66fb3f
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 16:01:35 +0800

    sets up a loop that iterates over each character in the plaintext, one at a time

--- Vignere Cipher.py
+++ Vignere Cipher.py
@@ -2,5 +2,6 @@
 def vigenere_cipher(plaintext,key):
     ciphertext= ""
     key_len= len(key)
+    for i, char in enumerate(plaintext):
 #plaintext-- error message will appear if it's not in all uppercase letters, no spaces
 #The ciphertext produced by encrypting plaintext using the Vigenère cipher with the given key.
